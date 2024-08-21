7645db1c35d069fad5fcbae084208c2f15f9dea5
Yammna <yammnanoor2000@gmail.com>
2024-08-21 22:17:49 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+//Navigation Bar
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+  
+    burger.addEventListener("click", () => {
+      //Toggle Nav
+      nav.classList.toggle("nav-active");
+  
+      //Animate Links
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.5
+          }s`;
+        }
+      });
+  
+      //Burger Animation
+      burger.classList.toggle("toggle");
+    });
+  };
+  
+  navSlide();
+  
+  //Contact Form
+  const inputs = document.querySelectorAll(".input");
+  
+  function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+  }
+  
+  function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+      parent.classList.remove("focus");
+    }
+  }
+  
+  inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+  });
